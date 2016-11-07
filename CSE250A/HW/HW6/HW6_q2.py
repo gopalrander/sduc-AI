b4a1aa6bf8de2a186e4624ede1c1ce5b099ef630
Gopal Rander <gopalmaheshwari07@gmail.com>
2016-11-07 00:04:47 -0800

    homework 6 250A

---
+++ CSE250A/HW/HW6/HW6_q2.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  6 12:32:29 2016
+
+@author: gopal
+"""
+
+import numpy as np
+
+OX = np.loadtxt('spectX.txt', dtype=int)
+OY = np.loadtxt('spectY.txt', dtype=int)
+
+PZ_X = np.zeros(OX.shape[1], dtype=int)
+PZ_X = PZ_X + (1/len(PZ_X))
+
+#P(Y|X)
+def getPY_X(PZ_X, X, Y):
+    result = 1.0
+    for i in range(len(PZ_X)):
+        result = result * (np.power((1-PZ_X[i]), X[i]))
+    if(Y==1):
+        result = 1-result
+    return result
+
+def LLhood(OX, OY, PZ_X):
+    llhood = 0.0
+    for i in range (len(OX)):
+        llhood = llhood + np.log(getPY_X(PZ_X, OX[i], OY[i]))
+    llhood = llhood/len(OX)
+    return llhood
+
+def countMistakes(PY_X):
+    return np.count_nonzero(PY_X<0.5)
+
+def EStep(OX,OY,PZ_X):
+    #PZX_XY
+    for loop in range(257):
+        PY_X = np.array([getPY_X(PZ_X, OX[i],OY[i]) for i in range(len(OX))])
+        a = (OX.T/PY_X).T * PZ_X
+        PnewZ_X = np.inner(OY, a.T)    
+        PnewZ_X = np.array([PnewZ_X[i]/np.count_nonzero(OX.T[i]) for i in range(len(PnewZ_X))])
+        if (loop & loop-1) == 0:
+            print (loop, countMistakes(PY_X), LLhood(OX, OY, PZ_X))
+        PZ_X = PnewZ_X
---
+++ CSE250A/HW/HW6/q3plots.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  6 20:15:00 2016
+
+@author: gopal
+"""
+import numpy as np
+import matplotlib.pyplot as plt 
+def g(x):
+    result = 0.0
+    for k in range(10):
+        result += np.log(np.math.cosh(x+ (1/(k+1))))
+    return result/10;
+
+def gd(x):
+    result=0.0
+    for k in range(10):
+        result += np.tanh(x + (1/(k+1)))
+    return result/10;
+    
+def f(x):
+    return np.log(np.cosh(x));
+
+def fd(x):
+    return np.tanh(x);
+
+def fdd(x):
+    return 1/(np.cosh(x) * np.cosh(x))
+
+def Q(x,y):
+    return f(y)+fd(y)*(x-y) + np.power((x-y),2)/2    
+
+def ConvergerAux(x0, loops, func, funcd):
+    x = x0;
+    it = np.zeros(loops)
+    xVal = np.zeros(loops)
+    for i in range(loops):
+        xVal[i]= x
+        it[i] = i
+        print (it[i], x)
+        x = x - funcd(x)
+    plt.plot(it, xVal)
+    plt.xlabel("iterations(n)")
+    plt.ylabel("x_n")
+    print (x, func(x))
+
+def ConvergerNewton(x0, loops):
+    x = x0;
+    it = np.zeros(loops)
+    xVal = np.zeros(loops)
+    for i in range(loops):
+        xVal[i]= x
+        it[i] = i
+        x = x - (fd(x)/fdd(x))
+    plt.plot(it, xVal)
+    plt.xlabel("iterations(n)")
+    plt.ylabel("x_n")
+    print (x, f(x))
+    
+def plot(l,r,step,func, y0=0):
+    xRange = np.arange(l,r,step)
+    if func==Q:
+        y = np.array([func(x,y0) for x in xRange])
+    else:
+        y = np.array([func(x) for x in xRange])
+    plt.plot(xRange,y)
+    plt.xlabel("x")
+    ylabel = func.__name__ + '(x'
+    if y0!=0: ylabel+= (', '+str(y0))
+    ylabel += ')'                               
+    plt.ylabel(ylabel)
+
